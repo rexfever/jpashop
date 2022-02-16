489cba0f76f71b979b620ed01a5f60d1ba80491e
rex <rex@rexui-MacBookPro.local>
2022-02-16 22:58:25 +0900

    merge

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,13 +1,15 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,13 +18,20 @@ public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
-        
+        member.setName("memberA");
         //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
 
         //then
-
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member " + (findMember == member));
     }
 }
